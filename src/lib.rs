e24c64494e0a7010e6fffb0f6c6dfd7b57b0a2ec
alimilhim <alimilhim5@gmail.com>
2020-02-22 01:27:10 +0200

    added borrowing support for key find

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use std::mem;
+use std::borrow::Borrow;
 
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
@@ -20,7 +21,10 @@ impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
 {
-    fn get_bucket_index(&self, key: &K) -> usize {
+    fn get_bucket_index<Q>(&self, key: &Q) -> usize
+    where K:Borrow<Q>, 
+    Q:Hash +Eq +?Sized 
+    {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         (hasher.finish() % (self.buckets.len() as u64)) as usize
@@ -41,27 +45,38 @@ where
         None
     }
 
-      pub fn contains_key(&self, key: &K) -> bool{
-        let bucket_index = self.get_bucket_index(key);
+      pub fn contains_key<Q>(&self, key: &Q) -> bool
+        where 
+        K:Borrow<Q>,
+        Q: Hash +Eq +?Sized
+      {
+    
+        let bucket_index = self.get_bucket_index(key.borrow());
         self.buckets[bucket_index]
             .iter()
-            .find(|&(ref ekey, _)| ekey == key)
+            .find(|&(ref ekey, _)| ekey.borrow() == key)
             .is_some()
       }
 
-      pub fn get(&self, key: &K) -> Option<&V> {
+      pub fn get<Q>(&self, key: &Q) -> Option<&V>
+      where
+      K:Borrow<Q>, 
+      Q:Hash +Eq +?Sized {
         let bucket_index = self.get_bucket_index(key);
         self.buckets[bucket_index]
             .iter()
-            .find(|&(ref ekey, _)| ekey == key)
+            .find(|&(ref ekey, _)| ekey.borrow() == key)
             .map(|&(_, ref eval)| eval)
     }
 
-    pub fn remove(&mut self, key:&K) -> Option<V>{
+    pub fn remove<Q>(&mut self, key:&Q) -> Option<V>
+      where
+      K:Borrow<Q>, 
+      Q:Hash +Eq +?Sized {
         let bucket_index = self.get_bucket_index(key);
         let bucket=&mut self.buckets[bucket_index]; 
         let element_index = bucket.iter()
-        .position(|&(ref ekey,_)| ekey ==key)?;
+        .position(|&(ref ekey,_)| ekey.borrow() ==key)?;
         self.items_size-=1;
         Some(bucket.swap_remove(element_index).1)
         }
@@ -91,7 +106,45 @@ where
         mem::replace(&mut self.buckets, new_buckets);
     }
 }
-impl<'a,K,V> IntoIterator for &'a &HashMap<K,V>{
+
+
+pub struct Iter<'a,K:'a,V:'a>{
+    map:&'a HashMap<K,V>,
+    bucket:usize,
+    at:usize,
+}
+
+impl<'a,K,V> IntoIterator for &'a HashMap<K,V>{
+    type Item = (&'a K,&'a V);
+    type IntoIter= Iter<'a,K,V>;
+    
+    fn into_iter(self)-> Self::IntoIter{
+        Iter{map:self,bucket:0,at:0}
+    }
+}
+
+impl<'a,K,V> Iterator for Iter<'a,K,V>{
+    type Item = (&'a K, &'a V);
+    fn next(&mut self)->Option<Self::Item>{
+        loop{
+            match self.map.buckets.get(self.bucket){
+                Some(bucket)=>{
+                    match bucket.get(self.at){
+                        Some(&(ref k,ref v))=>{
+                            self.at+=1;
+                            break Some((k,v));
+                        }
+                        None =>{
+                            self.bucket+=1;
+                            self.at=0;
+                            continue;
+                            }
+                    }
+                }
+                None => break None,
+            }
+        }
+    }
 }
 
 #[cfg(test)]
